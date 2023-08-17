42b372ba4717cbcd0663fda36729b6ff017c736b
PVRajeshRoyal <pvrajeshroyal@gmail.com>
2023-08-17 16:02:35 +0530

    Playwright Test V4.0

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,6 @@ const config = {
         headless: false,
         launchOptions: { slowMo: 50 },
         screenshot: 'only-on-failure',
-        video: 'on',
     },
     workers: 1, //By default all the test are run in parallel but we can limit the parallel run by keeping workers as 1
     reporter: 'allure-playwright',
